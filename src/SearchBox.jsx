03a9883af82956aa9d2245bc09b059747a62e11f
isharailanga <isharailanga9@gmail.com>
2018-07-21 02:03:11 +0530

    exclude componentNames based on componentType

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -14,13 +14,21 @@ class SearchBox extends Widget {
     this.handleChange = this.handleChange.bind(this);
     this.handleDataReceived = this.handleDataReceived.bind(this);
     this.getCurrentPage = this.getCurrentPage.bind(this);
-
+    this.getUrlParameter = this.getUrlParameter.bind(this);
+    this.excludeComponets = this.excludeComponets.bind(this);
+    this.publishMessage = this.publishMessage.bind(this);
+    this.pageName = this.getCurrentPage();
+    this.pgAPI = 'api';
+    this.pgEndpoint = 'endpoint';
+    this.pgProxy = 'proxy';
+    this.pgSequence = 'sequence';
   }
 
+
   getCurrentPage() {
-    var pageName;
-    var href = parent.window.location.href;
-    var lastSegment = href.substr(href.lastIndexOf('/') + 1);
+    let pageName;
+    let href = parent.window.location.href;
+    let lastSegment = href.substr(href.lastIndexOf('/') + 1);
     if (lastSegment.indexOf('?') == -1) {
       pageName = lastSegment;
     } else {
@@ -32,17 +40,21 @@ class SearchBox extends Widget {
   componentDidMount() {
 
     let query;
-    let pgAPI = 'api'; let pgEndpoint = 'endpoint'; let pgProxy = 'proxy';
-    let pageName = this.getCurrentPage();
+
+    var urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.has('id')) {
+      let selectedComp = this.getUrlParameter('id');
+      this.publishMessage(selectedComp);
+    }
 
     super.getWidgetConfiguration(this.props.widgetID)
       .then((message) => {
-        if (pageName == pgAPI || pageName == pgProxy || pageName == pgEndpoint) {
+        if (this.pageName == this.pgAPI || this.pageName == this.pgProxy || this.pageName == this.pgEndpoint) {
           query = message.data.configs.providerConfig.configs.config.queryData.queryESB;
         } else {
           query = message.data.configs.providerConfig.configs.config.queryData.queryMediator;
         }
-        message.data.configs.providerConfig.configs.config.queryData.query = query.replace('{{paramComponentType}}', '\'' + pageName + '\'');
+        message.data.configs.providerConfig.configs.config.queryData.query = query.replace('{{paramComponentType}}', '\'' + this.pageName + '\'');
         super.getWidgetChannelManager().subscribeWidget(this.props.id, this.handleDataReceived, message.data.configs.providerConfig);
       })
       .catch((error) => {
@@ -53,23 +65,61 @@ class SearchBox extends Widget {
   }
 
   handleDataReceived(data) {
+
+    var componentNameArr = data.data.map(
+      function (nameArr) {
+        return nameArr[0];
+      });
+
+    if (this.pageName == this.pgEndpoint) {
+      let excludeSequences = ["AnonymousEndpoint"];
+      this.excludeComponets(componentNameArr, excludeSequences);
+    }
+
+    else if (this.pageName == this.pgSequence) {
+      let excludeSequences = ["PROXY_INSEQ", "PROXY_OUTSEQ", "PROXY_FAULTSEQ", "API_OUTSEQ", "API_INSEQ", "API_FAULTSEQ", "AnonymousSequence"];
+      this.excludeComponets(componentNameArr, excludeSequences);
+    }
+
     this.setState({
-      optionArray: data.data.map(suggestion => ({
-        value: suggestion[0],
-        label: suggestion[0],
+      optionArray: componentNameArr.map(option => ({
+        value: option,
+        label: option,
       }))
     });
   }
 
 
+  excludeComponets(componentNameArr, excludeSequences) {
+    let item;
+    for (item in excludeSequences) {
+      let exSeq = excludeSequences[item];
+      let index = componentNameArr.indexOf(exSeq);
+      if (index > -1) {
+        componentNameArr.splice(index, 1);
+      }
+    }
+  }
+  getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+  };
+
   handleChange(event) {
     if (event) {
       let selectedValue = event.value;
-      let selectedComponent = { "selectedComponent": selectedValue };
-      this.setState({ selectedOption: selectedValue });
-      this.publishedMsgSet.push({ time: new Date(), value: selectedValue });
-      super.publish(selectedComponent);
+      this.publishMessage(selectedValue);
     }
+  }
+
+  publishMessage(pubMessage) {
+    this.setState({ selectedOption: pubMessage });
+    let selectedComponent = { "selectedComponent": pubMessage };
+    this.publishedMsgSet.push({ time: new Date(), value: pubMessage });
+    super.publish(selectedComponent);
+    //super.publish(JSON.stringify(selectedComponent));
   }
 
 
